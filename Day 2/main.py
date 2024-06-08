36e91605c2b9b7111e891873e38619e819c6ceca
jianxunbak <169042449+jianxunbak@users.noreply.github.com>
2024-06-08 15:43:07 +0800

    Delete Day 2 directory

--- Day 2/main.py
+++
@@ -1,8 +0,0 @@
-print("welcome to the tip calculator")
-total_bill = float(input("what was the total bill?"))
-percent = float(input("how much tip would you like to give? 10, 12 or 15?"))
-Nos = float(input("how many people to split the bill with?"))
-tip = (total_bill) * ((percent / 100) + 1) / Nos
-final = "{:.2f}".format(tip)
-print(f"Each person should pay: ${final}")
-
